24827938dc816a1f6a780509047610cd08366f01
Robert Maples <robertmaples@Marcuss-MBP-2.localdomain>
2022-03-02 11:44:04 -0500

    apollo server up and running, getById not working

--- index.js
+++ index.js
@@ -1,49 +1,70 @@
-const hapi = require('@hapi/hapi');
-const mongoose = require('mongoose');
-const { apolloHapi, graphiqlHapi } = require('apollo-server');
-const { makeExecutableSchema } = require('@graphql-tools/schema');
+const { ApolloServer, ApolloServerPluginStopHapiServer, gql } = require('apollo-server-hapi')
+const hapi = require('@hapi/hapi')
+const { v4: uuidv4 } = require('uuid');
 
-const Counter = require('./models/counter');
-
-const graphqlSchema = require('./graphql/schema');
-const createResolvers = require('./graphql/resolvers');
-
-const server = hapi.Server({
-    port: 8080,
-    host: 'localhost'
-});
-
-mongoose.connect('mongodb://localhost:27017/test')
-
-const executableSchema = makeExecutableSchema({
-   typeDefs: [graphqlSchema],
-    resolvers: createResolvers({ Counter })
-});
-
-server.register({
-    register: apolloHapi,
-    options: {
-        path: '/graphql',
-        apolloOptions: () => ({
-            pretty: true,
-            schema: executableSchema,
-        }),
-    },
-});
-
-server.register({
-    register: graphiqlHapi,
-    options: {
-        path: '/graphiql',
-        graphiqlOptions: {
-            endpointUrl: '/graphql'
-        }
-    },
-});
-
-server.start((err) => {
-    if (err) {
-        throw err;
+const typeDefs = gql`
+    type Counter {
+        id: String!    
+        value: Int!
     }
-    console.log(`Server running at: ${server.info.url}`);
-});
+    type Query {
+        getCounterById(id: String!): Counter
+        getCounters: [Counter]
+    }
+    type Mutation {
+        createCounter: Counter
+        incrementCounter(id: String!): Counter
+    }
+`;
+
+const counters = [
+    {
+        id: '9d0a8a68-f9a4-4464-9619-980c9db85eff',
+        value: 100
+    },
+    {
+        id: '33d184b3-0bdf-4c5f-8cc3-b930efadb1bd',
+        value: 50,
+    },
+];
+
+// Resolvers define the technique for fetching the types defined in the
+// schema. This resolver retrieves books from the "books" array above.
+const resolvers = {
+    Query: {
+        getCounterById: (id) => {
+            console.log('id', id, 'counters.find', counters.find(id))
+            counters.find(id)
+        },
+        getCounters: () => counters
+    },
+    Mutation: {
+        createCounter: () => {
+            const counter = {id: uuidv4(), value: 0}
+
+            counters.push(counter)
+            return counter
+        },
+        incrementCounter: (id) => {
+            const counter = counters.find(id);
+
+            counter.value = counter.value + 1;
+            return counter;
+        }
+    }
+};
+
+async function startApolloServer(typeDefs, resolvers) {
+    const app = hapi.server({ port: 4000 });
+    const server = new ApolloServer({
+        typeDefs,
+        resolvers,
+        plugins: [ApolloServerPluginStopHapiServer({ hapiServer: app })],
+    });
+
+    await server.start();
+    await server.applyMiddleware({ app });
+    await app.start();
+}
+
+startApolloServer(typeDefs, resolvers)
